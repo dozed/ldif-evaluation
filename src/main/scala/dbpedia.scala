04f30b690039210bba3ceff2b6490b3c5ccb0ff7
Stefan Ollinger <Stefan.Ollinger@gmx.de>
2013-10-11 13:06:57 +0200

    Created a Stream-based SPARQL exporter.

--- src/main/scala/dbpedia.scala
+++ src/main/scala/dbpedia.scala
@@ -1,5 +1,7 @@
 import dispatch._, Defaults._
+import scala.concurrent.Await
 import scala.xml.Elem
+import scala.concurrent.duration._
 
 object DBpedia {
 
@@ -23,3 +25,65 @@ object DBpedia {
 
 }
 
+
+case class SparqlEndpoint(uri: String) {
+
+  val svc = url(uri)
+
+  def dump(query: String, pageSize: Int = 5000, offset: Long = 0, retry: Int = 0, retryMax: Int = 10): Stream[String] = {
+    var q = query
+    q += " OFFSET " + offset
+    q += " LIMIT " + pageSize
+    val qp = Map("query" -> q, "format" -> "text/turtle")
+
+    println(q)
+
+    val f = Http(svc <<? qp OK as.String) map (_.split("\n").toList) either
+
+    Await.result(f, 2 minutes) match {
+      case Left(error) =>
+        if (retry < retryMax) {
+          dump(query, pageSize, offset, retry + 1)
+        } else {
+          throw error
+        }
+      case Right(lines) =>
+        if (lines.size == 0 || (lines.size == 1 && lines(0).equals("# Empty NT"))) {
+          Stream.empty
+        } else {
+          lines.toStream #::: dump(query, pageSize, offset + pageSize)
+        }
+    }
+  }
+
+}
+
+object SparqlImporter extends App {
+
+  val query = """CONSTRUCT {
+                |  ?b rdfs:label ?label .
+                |} WHERE {
+                |  ?b dcterms:subject ?x .
+                |  OPTIONAL {
+                |    ?b foaf:name ?label .
+                |    ?b rdfs:label ?label .
+                |    ?b dbpprop:name ?label .
+                |  }
+                |  {
+                |    SELECT ?x WHERE { ?x skos:broader* category:Foods . }
+                |  } UNION {
+                |    SELECT ?x WHERE { ?x skos:broader* category:Beverages . }
+                |  }
+                |}
+                |""".stripMargin
+
+  val pw = new java.io.PrintWriter("foods.nt")
+  val endpoint = SparqlEndpoint("http://dbpedia.org/sparql")
+  var k = 0
+  endpoint.dump(query, offset = 65000) foreach { line =>
+    println(line)
+    pw.println(line)
+  }
+  pw.close
+
+}

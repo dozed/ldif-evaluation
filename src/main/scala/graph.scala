35fa34cb609e707638f348a4494b43881cd07f76
Stefan Ollinger <Stefan.Ollinger@gmx.de>
2013-11-26 00:54:39 +0100

    Finer data.

--- src/main/scala/graph.scala
+++ src/main/scala/graph.scala
@@ -1293,15 +1293,15 @@ object TestDataset {
 //      DenseVector(0, 0, 0, 0, 1, 0, 0, 0),
 //      DenseVector(0, 0, 0, 0, 0, 1, 0, 0),
 //      DenseVector(0, 0, 0, 0, 0, 0, 1, 0)
-      DenseVector(0, 1, 1, 1, 1, 1, 0, 0), // aggregated string measures
-      DenseVector(0, 1, 0, 1, 0, 1, 0, 0),
-      DenseVector(0, 1, 0, 0, 0, 1, 0, 0),
-      DenseVector(0, 0, 0, 1, 0, 1, 0, 0)
-//      DenseVector(0, 1, 0, 0, 0, 1, 1, 0), // aggregated all measures
-//      DenseVector(0, 1, 0, 1, 0, 1, 1, 0),
-//      DenseVector(0, 0, 0, 1, 0, 1, 1, 0),
-//      DenseVector(0, 1, 0, 1, 0, 0, 1, 0),
-//      DenseVector(0, 1, 0, 0, 0, 3, 1, 0)  // non-binary weights
+//      DenseVector(0, 1, 1, 1, 1, 1, 0, 0), // aggregated string measures
+//      DenseVector(0, 1, 0, 1, 0, 1, 0, 0),
+//      DenseVector(0, 1, 0, 0, 0, 1, 0, 0),
+//      DenseVector(0, 0, 0, 1, 0, 1, 0, 0)
+      DenseVector(0, 1, 0, 0, 0, 1, 1, 0), // aggregated all measures
+      DenseVector(0, 1, 0, 1, 0, 1, 1, 0),
+      DenseVector(0, 0, 0, 1, 0, 1, 1, 0),
+      DenseVector(0, 1, 0, 1, 0, 0, 1, 0),
+      DenseVector(0, 1, 0, 0, 0, 3, 1, 0)  // non-binary weights
     )
 
 //    val S2 = for {
@@ -1326,7 +1326,7 @@ object TestDataset {
     } yield {
       val l = labelWeights(s)
       val idx = i*A.size+j+1
-      val pw = new PrintWriter(f"ldif-taaable/grain/agg-string-${idx}.csv")
+      val pw = new PrintWriter(f"ldif-taaable/grain/agg-all-${idx}.csv")
       pw.println(f"# $l-$al")
       val r = stats(a, s).toList
       r map product2csv foreach pw.println
